69d7cf0513511c09929799ba4d30a61b7e8e6915
mvvassilev <m.v.vassilev@abv.bg>
2019-11-16 22:03:58 +0800

    Search functionality now displays 20 formated dummy results

--- recommender/views.py
+++ recommender/views.py
@@ -5,3 +5,17 @@ from . import models
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+
+    results = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+    context = {
+        'search': search,
+        'results': results
+    }
+    return render(request, 'recommender/new_search.html', context)
