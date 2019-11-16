439c96f7d6c45ece03123771f85a1be127dd6e4e
mvvassilev <m.v.vassilev@abv.bg>
2019-11-17 02:53:35 +0800

    Surved to Heroku at soundplug.herokuapp.com

--- recommender/views.py
+++ recommender/views.py
@@ -12,7 +12,7 @@ def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
 
-    results = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    results = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
     context = {
         'search': search,
